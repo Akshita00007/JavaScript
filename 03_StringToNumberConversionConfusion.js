142a1ccaa7ce62dd2fd2fbe9220f2da70c0bc633
Akshita Mukati <136486348+Akshita00007@users.noreply.github.com>
2023-07-30 18:20:32 +0530

    Add files via upload

---
+++ 03_StringToNumberConversionConfusion.js
@@ -0,0 +1,37 @@
+// *****************************Operations ***********************
+let value=33
+let negValue=-value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1="hello"
+let str2=" akshita"
+
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2+2)   //122
+console.log(2+2+"1")   //41
+console.log(1+"2")
+console.log("1"+"2")
+
+console.log(true)   //true
+console.log(+true)  //1
+// console.log(true+)  //error
+console.log(+"")
+
+let num1,num2,num3 
+num1=num2=num3=2+2
+console.table({num1,num2,num3})
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
+  
+ 
