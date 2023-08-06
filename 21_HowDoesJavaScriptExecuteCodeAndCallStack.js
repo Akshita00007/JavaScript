ca13587dc4006dc4186f8e822751998b0eb6b1a3
Akshita Mukati <136486348+Akshita00007@users.noreply.github.com>
2023-08-06 12:59:48 +0530

    Add files via upload

---
+++ 21_HowDoesJavaScriptExecuteCodeAndCallStack.js
@@ -0,0 +1,13 @@
+// ******************* How does JavaScript Execute Code and Call Satck **************************
+function one(){
+    console.log("One");
+}
+function two(){
+    console.log("two");
+}
+function three(){
+    console.log("three");
+}
+one();
+two();
+three();
