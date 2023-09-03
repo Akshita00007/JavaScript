0d5d6d39fd6029dc76d9d2dfe5f632003231f603
Akshita Mukati <136486348+Akshita00007@users.noreply.github.com>
2023-09-03 16:04:40 +0530

    Add files via upload

---
+++ 26_AsyncJavascriptFundamentals.js
@@ -0,0 +1,13 @@
+//***************** Async JavaScript Fundamentals **********************
+// javascript is synchronous
+// javascript is single threaded -- ek hi thread pe sb execute hoga
+// default javascript behaviour
+
+// Execution Context -- 
+// execute one line of code at a time
+// each operation wait for the last one to complete before xecuting
+
+// Blocking Code vs Non Blocking Code
+// Blocking Code-- Block the flow of the program Read File Sync
+// Non Blocking Code-- Read File Async
+// depends on use cases
